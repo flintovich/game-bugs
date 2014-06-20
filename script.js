2e6b0807ae4f27ffdb6f0c426ef10b39c81d0f10
Ruslan Sukhorukov <flintovich@gmail.com>
2014-06-20 15:47:25 +0300

    added second bug

--- script.js
+++ script.js
@@ -67,77 +67,77 @@ window.onload=function(){
         ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
     }
     function CircleCreation(ctx, x, y, radius){
-        this.ctx = ctx;
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, Math.PI*2, true);
         ctx.closePath();
         ctx.fill();
     }
-    CircleCreation.prototype.move = function(randX,randY){
-        var i = CreatorElements.rand(0,3);
-        if(i==0){
-            if(randY<=50) return randY;
-            return randY = randY-2;
-        } else if(i==1) {
-            if(randX>=width-50) return randX;
-            return randX = randX+2;
-        } else if(i==2) {
-            if(randY>=height-50) return randY;
-            return randY = randY+2;
-        } else if(i==3) {
-            if(randX<=50) return randX;
-            return randX = randX-2;
-        }
-    };
 
+    // Смещение жука по клику
+    var x,y;
+    canvas.onmousemove = function(e){
+        x = e.clientX - (canvas.offsetParent.offsetLeft + canvas.offsetLeft);
+        y = e.clientY - (canvas.offsetParent.offsetTop + canvas.offsetTop);
+
+        randX = randX+((x-randX)/150);
+        randY = randY+((y-randY)/150);
+    };
+    function randMove(axis, bugSpeed){
+        if(!bugSpeed) bugSpeed = 2;
+        var i = CreatorElements.rand(0,1,2);
+        if(i==0){
+            if(axis>=width-50) return axis;
+            axis = axis+bugSpeed;
+            return axis;
+        } else if(i==1) {
+            if(axis<=50) return axis;
+            axis = axis-bugSpeed;
+            return axis;
+        }
+        else if(i==2) {
+            return false;
+        }
+    }
+
+    function createBadBugs(count){
+        var randBugX = CreatorElements.rand(150,height-250);
+        var randBugY = CreatorElements.rand(150,height-250);
+        (function(){
+            for(var i=0; i<count; i++){
+                CreatorElements.circle(ctx, randBugX+count, randBugY+count, 4, 220, 0,0, 9);
+                randBugX = randMove(randBugX, CreatorElements.rand(1,6));
+                randBugY = randMove(randBugY, CreatorElements.rand(1,6));
+            }
+        })();
+    }
 
     // Стартовые координаты жука
     var randX = CreatorElements.rand(50,width-50);
     var randY = CreatorElements.rand(50,height-50);
-
-    // Смещение жука по клику
-    var x,y;
-    canvas.onclick = function(e){
-        x = e.clientX - (canvas.offsetParent.offsetLeft + canvas.offsetLeft);
-        y = e.clientY - (canvas.offsetParent.offsetTop + canvas.offsetTop);
-
-        randX = randX+((x-randX)/10);
-        randY = randY+((y-randY)/10);
-    };
-
+    var randBugX = CreatorElements.rand(150,height-250);
+    var randBugY = CreatorElements.rand(150,height-250);
     function start(){
         clear(ctx);
 
         // Рисуем нашего жука
-        //CreatorElements.circle(ctx, randX, randY, 3);
-        var bug = new CircleCreation(ctx, randX, randY, 3);
-        console.log(bug.move)
+        CreatorElements.circle(ctx, randX, randY, 6, 0, 0,0, 9);
+        randX = randMove(randX);
+        randY = randMove(randY);
 
-        // Задаем условия смещения жука
-        /*var i = CreatorElements.rand(0,3);
-        if(i==0){
-            if(randY<=50) return randY;
-            return randY = randY-2;
-        } else if(i==1) {
-            if(randX>=width-50) return randX;
-            return randX = randX+2;
-        } else if(i==2) {
-            if(randY>=height-50) return randY;
-            return randY = randY+2;
-        } else if(i==3) {
-            if(randX<=50) return randX;
-            return randX = randX-2;
-        }*/
+        // Рисуем чужего жука
+        /*CreatorElements.circle(ctx, randBugX, randBugY, 4, 220, 0,0, 9);
+        randBugX = randMove(randBugX, CreatorElements.rand(1,6));
+        randBugY = randMove(randBugY, CreatorElements.rand(1,6));
+        */
+        createBadBugs(2);
+
     }
 
-    (function animationLoop(){
+    /*(function animationLoop(){
         start();
         requestAnimationFrame(animationLoop,'#canvas');
-    })();
+    })();*/
+    setInterval(start, 40);
 
 
 };
